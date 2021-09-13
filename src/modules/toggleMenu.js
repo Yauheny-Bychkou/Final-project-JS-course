c53a1c15af7d9dacd5617f46d10d03630a1ec62d
Yauheny Bychkou <375445433672@yandex.by>
2021-09-13 14:56:05 +0300

    add modal in header, nav

--- src/modules/toggleMenu.js
+++ src/modules/toggleMenu.js
@@ -1,6 +1,16 @@
 const toggleMenu = () => {
   const overlay = document.querySelector(".overlay");
   const headerModal = document.querySelector(".header-modal ");
+  const navbar = document.querySelector(".navbar");
+
+  window.addEventListener("scroll", function (e) {
+    if (pageYOffset >= 214.39999389648438) {
+      navbar.style.margin = "0";
+    } else {
+      navbar.style.marginTop = "-60px";
+    }
+  });
+
   document.addEventListener("click", (event) => {
     let target = event.target;
     if (target.classList.contains("headerModal")) {
@@ -9,6 +19,9 @@ const toggleMenu = () => {
     } else if (target.classList.contains("header-modal__close")) {
       headerModal.classList.toggle("header-modal");
       overlay.style.display = "none";
+    } else if (target.closest(".navbar") && target.classList.contains("fancyboxModal")) {
+      headerModal.classList.toggle("header-modal");
+      overlay.style.display = "initial";
     }
   });
 };
